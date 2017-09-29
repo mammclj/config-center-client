a19691df492ef9a9dab2c9ae7f480f03f3043b7c
mammclj <869840420@qq.com>
2017-09-29 14:11:53 +0800

    springcloud的配置中心客户端demo

---
+++ src/test/java/com/msymobile/payfull/configcenterclient/ConfigCenterClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.msymobile.payfull.configcenterclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigCenterClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
